f31882e0bab45d28ebcb9d18472ca0904b781a79
E3FxGaming <8276268+E3FxGaming@users.noreply.github.com>
2022-12-12 12:35:04 +0100

    day 12 - afterthought polishing

--- src/main/kotlin/org/example/e3FxGaming/adventOfCode2022Kotlin/Day12.kt
+++ src/main/kotlin/org/example/e3FxGaming/adventOfCode2022Kotlin/Day12.kt
@@ -3,10 +3,10 @@ package org.example.e3FxGaming.adventOfCode2022Kotlin
 import java.io.Reader
 
 class Day12(override val inputReader: Reader) : Day {
-    private val field = inputReader.closingReadLines()
+    private val field: Array<CharArray> = inputReader.closingReadLines()
         .map(String::toCharArray).toTypedArray()
 
-    val goalPair: Pair<Int, Int> = kotlin.run {
+    private val goalPair: Pair<Int, Int> = kotlin.run {
         val goalRow = field.indexOfFirst { it.contains('E') }
         val goalCol = field[goalRow].indexOfFirst { it == 'E' }
         goalRow to goalCol
@@ -22,18 +22,18 @@ class Day12(override val inputReader: Reader) : Day {
     }
 
     override fun part2(): Int {
-        val startingSet = mutableSetOf<Pair<Int, Int>>()
-
-        field.forEachIndexed { rowIndex, line ->
-            line.forEachIndexed { colIndex, elevation ->
-                if (elevation == 'a') startingSet.add(rowIndex to colIndex)
+        val startingSet = buildSet {
+            field.forEachIndexed { rowIndex, line ->
+                line.forEachIndexed { colIndex, elevation ->
+                    if (elevation == 'a') add(rowIndex to colIndex)
+                }
             }
         }
 
         return goHiking(startingSet)
     }
 
-    fun goHiking(startingToDo: Set<Pair<Int, Int>>): Int {
+    private fun goHiking(startingToDo: Set<Pair<Int, Int>>): Int {
         val visited = Array(field.size) {
             BooleanArray(field.first().size)
         }
@@ -51,18 +51,21 @@ class Day12(override val inputReader: Reader) : Day {
         while (toDo.isNotEmpty()) {
             steps += 1
             toDo = toDo.flatMap { (currentRow, currentCol) ->
-                val currentFieldValue = field[currentRow][currentCol].let { if (it == 'S') 'a' else it }
+                val currentElevationChar = field[currentRow][currentCol].let { if (it == 'S') 'a' else it }
                 dirs.map {
                     currentRow + it.first to currentCol + it.second
                 }.filter { (newRow, newCol) ->
                     newRow in rowIndices && newCol in colIndices
                             && !visited[newRow][newCol]
-                            && ((currentFieldValue >= 'y' && field[newRow][newCol] == 'E') || field[newRow][newCol] in 'a'..(currentFieldValue + 1))
+                            && ((currentElevationChar >= 'y' && field[newRow][newCol] == 'E') || field[newRow][newCol] in 'a'..(currentElevationChar + 1))
                 }
-            }.toSet().onEach { nextPosition ->
-                if (goalPair == nextPosition) return steps
-                visited[nextPosition.first][nextPosition.second] = true
-            }
+            }.toSet()
+                .also {
+                    if (it.contains(goalPair)) return steps
+                }
+                .onEach { nextPosition ->
+                    visited[nextPosition.first][nextPosition.second] = true
+                }
         }
 
         throw IllegalStateException("Goal can't be reached")
